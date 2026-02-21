803cc2581afc158ea52bfb9086ccaaf0d23a9180
Gautam Kumar <gautam14cs@gmail.com>
2026-02-21 15:29:09 +0530

    Initial Commit

---
+++ Core-Java/JDK-8/OPtionalClass.java
@@ -0,0 +1,5 @@
+public class OPtionalClass {
+    public static void main(String[] args) {
+        System.out.println(" Hello !!!");
+    }
+}
---
+++ DSA/ArrayPrint.java
@@ -0,0 +1,5 @@
+public class ArrayPrint {
+    public static void main(String[] args) {
+        System.out.println(" Araray... ");
+    }
+}
