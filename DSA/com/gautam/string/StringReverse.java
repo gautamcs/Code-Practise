1627c37c1c17c91d168c02eb457cee5a943288ea
Gautam Kumar <gautam14cs@gmail.com>
2026-02-21 16:15:24 +0530

    Initial package

--- DSA/com/gautam/string/StringReverse.java
+++ DSA/com/gautam/string/StringReverse.java
@@ -3,10 +3,10 @@ package com.gautam.string;
 public class StringReverse {
     public static void main(String[] args) {
         String str = "Hello";
-        String temp = "";
+        StringBuffer temp = new StringBuffer();
         char[] ch  = str.toCharArray();
         for(int i = ch.length-1; i >=0; i--) {
-            temp = temp+ch[i];
+            temp = temp.append(ch[i]);
         }
 
         System.out.println(" Reverse String: "+temp);
