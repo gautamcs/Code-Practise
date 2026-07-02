3cdb0215c985801c165929c736fa547d9d94d597
Gautam Kumar <gautam14cs@gmail.com>
2026-07-02 23:56:40 +0530

    Data Change

--- Core-Java/ArrayPrint.java
+++ Core-Java/ArrayPrint.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class ArrayPrint {
     public static void main(String[] args) {
         
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1,2,3,4,5,6,7,9,0};
 
         System.out.println("Original Data: "+Arrays.toString(arr));
     }
