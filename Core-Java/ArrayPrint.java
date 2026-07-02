54b4d4a265d3c540c44a1ac077769b1214e2df33
Gautam Kumar <gautam14cs@gmail.com>
2026-07-02 23:51:42 +0530

    Initial Setup-1

---
+++ Core-Java/ArrayPrint.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArrayPrint {
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5,6,7};
+
+        System.out.println("Original Data: "+Arrays.toString(arr));
+    }
+}
